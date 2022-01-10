5379fbaee80ce14244e4b0af487a9be95f9d22f8
Vitor Felix <vitor_fellix@hotmail.com>
2022-01-10 11:33:23 -0300

    Added buttons and useState variables

--- financeapp/src/components/Transactions.js
+++ financeapp/src/components/Transactions.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Transactions = () => {
+  const [transactions, setTransactions] = useState(
+    [
+      {id: 1, name: "salario", value: 1000, date: "11/11/2021", optional: "Décimo terceiro"},
+      {id: 2, name: "salario", value: 200, date: "12/12/2021", optional: "Janeiro"}
+    ]
+  )
   return (
     <div className="transactions">
-      <p>Transactions be like</p>
+      {transactions.map((transaction) => (
+        <div className="transaction" key={transaction.id}>
+          <h2>{transaction.name}</h2>
+          <p>R${transaction.value} at {transaction.date}</p>
+          <p>{transaction.optional}</p>
+        </div>
+      ))}
     </div>
   );
 };
